50a9aa76bd96bd0de57beac0880c70db583efc2a
Sanohit Madaan <skumar2216.eace19@chitkara.edu.in>
2023-05-09 14:53:51 +0530

    started js

---
+++ index.js
@@ -0,0 +1,84 @@
+// // // // // console.log('hello world');
+
+// // // // // //variable
+// // // // // var a='sanohit madaan';
+// // // // // console.log(a);
+
+// // // // // //data types
+// // // // // console.log(typeof(a));
+// // // // // var a=7;
+// // // // // console.log(typeof(a));
+// // // // // var b=true;
+// // // // // console.log(typeof(b));
+
+// // // // // //challenge
+// // // // // console.log(3**3);
+// // // // // console.log(5+'Madaan');
+
+// // // // // swap two number
+// // // // var a=5;
+// // // // var b=10;
+// // // // var c=b;
+// // // // b=a;
+// // // // a=c;
+// // // // console.log("the vale of a is :" +a);
+// // // // console.log("the value of b is :"+ b);
+
+// // // // // without using third variable
+// // // // var a=5;
+// // // // var b =10;
+// // // // a=a+b;
+// // // // b=a-b;
+// // // // a=a-b;
+// // // // console.log("a is :"+a);
+// // // // console.log("b is :"+b);
+
+// // // // leap year
+// // // var a=2020;
+// // // if (a%4==0){
+// // //     if(a%100==0){
+// // //         if(a%400==0){
+// // //             console.log("The year "+a+" is a leap year");
+// // //         }
+// // //         else{
+// // //         console.log("The year "+a+" is a leap year");
+// // //     }
+// // //     }
+// // //     else{
+// // //         console.log("The year "+a+"is  a leap year");
+// // // }
+// // // }
+
+// // // else{
+// // //     console.log("The year "+a+"is not a leap year");
+// // // }
+
+// // // //switch cases 
+// // var area='square';
+// // var pi=3.14,l=2,b=2,r=2;
+
+// // switch(area){
+// //     case 'circle':
+// //         console.log("The area of circle is :"+ pi*r**2);
+// //         break
+
+// //         case 'rectangle':
+// //             console.log("the area of rectangle is: "+(l*b));
+// //             break
+
+// //             case 'triangle':
+// //                 console.log("the area of triangle is: "+(l*b)/2);
+// //                 break
+
+// //                 default:
+// //                     console.log("enter the valid data");
+// // }
+
+// //table for 8,9 using for loop
+// var a;
+// var table=9;
+// for(a=1;a<=10;a++)
+// {
+//     console.log(table +"*"+a+"="+table*a);
+// }
+
